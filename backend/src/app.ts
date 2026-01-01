498aa4e6b79a5f4abc03936404b1c9866ef9a0ab
Sahil Khan <salmanengis6397@gmail.com>
2026-01-01 22:48:39 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import chatRoutes from "./routes/chat.routes";
+import path from "path";
 
 const app = express();
 
@@ -14,7 +15,20 @@ app.use(
 );
 
 app.use(express.json());
-//app.options("*", cors());
 app.use("/chat", chatRoutes);
+
+// Serve frontend build
+const frontendPath = path.join(
+  __dirname,
+  "..",
+  "dist"
+);
+console.log("Serving frontend from:", frontendPath);
+app.use(express.static(frontendPath));
+
+
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
 
 export default app;
